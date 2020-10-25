6ec8cf9187c7e3a05a1b173e45c68f21788ed2ab
notmario <59957544+notmario@users.noreply.github.com>
2020-10-25 16:36:02 +0800

    still surprised

--- index.js
+++ index.js
@@ -387,12 +387,11 @@ pieceToMoves.unshift(function(x,y){});
 
 pieceToImage = [19,15,16,17,18,20,1,2,3,4,5,6,7,8,9,10,11,12,13,14,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55];
 
-pieceToImage.unshift(0);
-
 if (!standard) {
     pieceToImage = shuffle(pieceToImage);
 
 }
+pieceToImage.unshift(0);
 
 for (var i = 0; i < boardWidth; i++) {
     board[boardHeight-2][i] = 2;
