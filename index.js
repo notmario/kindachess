23c8e9fe3d1b1a48f9386496518b125d4b02aebf
notmario <59957544+notmario@users.noreply.github.com>
2020-10-25 16:32:18 +0800

    a suprise fix to the surprise tool

--- index.js
+++ index.js
@@ -256,7 +256,6 @@ function isNSpacesFromStart(piece,y,n) {
     }
 }
 
-pieceToImage.unshift(0);
 
 pieceToMoves = [function(x,y){leaper(x,y,1,1);leaper(x,y,1,0);leaper(x,y,0,1);},
     function(x,y) {
@@ -387,6 +386,8 @@ for (i=0; i<= 100; i++) {
 pieceToMoves.unshift(function(x,y){});
 
 pieceToImage = [19,15,16,17,18,20,1,2,3,4,5,6,7,8,9,10,11,12,13,14,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55];
+
+pieceToImage.unshift(0);
 
 if (!standard) {
     pieceToImage = shuffle(pieceToImage);
